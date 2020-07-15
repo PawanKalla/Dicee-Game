be8e3a971333056cf3800ca273ed0b2941c0085a
Pawan kalla <pawankalla2011@gmail.com>
2020-07-15 17:46:57 +0530

    2nd commit

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-(function() {
-document.getElementsByClassName('img1').setAttribute('src', 'images/dice1.png');
-})();
+// this section is to change the image on each reload
+
+var randomNO1= "images/" + "dice" + Math.ceil(Math.random()*6)  + ".png";
+document.getElementById('img1').setAttribute('src',randomNO1);
+
+var randomNO2= "images/" + "dice" + Math.ceil(Math.random()*6)  + ".png";
+document.getElementById('img2').setAttribute('src',randomNO2);
+
+// this section is to show which player wins
+if(randomNO1>randomNO2){
+  document.getElementsByTagName('h1')[0].innerHTML = "😈Player 1 wins🚩";
+}
+else if(randomNO2>randomNO1){
+  document.getElementsByTagName('h1')[0].innerHTML = "🚩Player 2 wins😈";
+}
+else{
+  document.getElementsByTagName('h1')[0].innerHTML = "😒🤝Draw🤝😞";
+}
